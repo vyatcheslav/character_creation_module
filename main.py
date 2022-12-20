7bff7bb2ae8c772c2e90cc525adf59fba4e84fd6
Вячеслав Тихонов <vyatcheslavtikhonov@yandex.ru>
2022-12-20 23:58:44 +0300

    Удалены лишние файлы и папки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -86,3 +87,16 @@ def choice_char_class() -> str:
                                'или любую другую кнопку, чтобы выбрать '
                                'другого персонажа ').lower()
     return char_class
+
+
+if __name__ == '__main__':
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
